db08df4b55179f67ea47be6b9eb1eca7f1c0b63b
ThaddG <thaddious.gorges@gmail.com>
2021-02-04 19:09:53 -0500

    starting over

--- src/APIs/noaaApi.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://www.ncdc.noaa.gov/cdo-web/api/v2',
-  headers: {
-    'Content-Type': 'application/json',
-    token: process.env.REACT_APP_NOAA_TOKEN,
-  },
-});
