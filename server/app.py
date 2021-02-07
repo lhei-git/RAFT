e6d6dea759bc0019384f0b24f08b61f5f6678474
Max Hendricks <maxhendricks24@gmail.com>
2021-02-07 12:48:08 -0500

    Some changes to json to csv

--- server/app.py
+++ server/app.py
@@ -2,6 +2,8 @@ import pickle
 import sys
 import json
 import requests
+import csv
+from pandas import json_normalize
 # from src import lookup_list
 from flask import Flask, render_template, request, jsonify, Response
 from flask_cors import CORS
@@ -24,7 +26,6 @@ def getStationData():
         fips = ll[request.args["state"]][request.args["county"]]
         fips = str(fips)
         reqString = "https://www.ncdc.noaa.gov/cdo-web/api/v2/stations?locationid=FIPS:" + fips + "&limit=1000&datasetid=GHCND"
-        print(reqString)
         response = requests.get(reqString, headers={'Token': 'iNyYxajqDfjhrLqStUAbIaddioahKEus'})
         if(json.loads(response.text) == None):
             return jsonify("NOAA's API can't be accessed"), 400
@@ -63,10 +64,39 @@ def counties():
         print(qs)
         return '[BAD REQUEST] {}'.format(e), 400
 
+@app.route('/')
+def test():
+    print("Entered Test")
+    reqString = "https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datatypeid=TAVG&datatypeid=TMIN&datatypeid=TMAX&datasetid=GSOM&startdate=1891-09-01&enddate=1901-09-01&locationid=FIPS:26093&limit=1000&stationid=GHCND:USC00203947"
+    response = requests.get(reqString, headers={'Token': 'iNyYxajqDfjhrLqStUAbIaddioahKEus'})
+    data = json.loads(response.text)
+    temp_data = data['results']
+    data_file = open('testing/data_file.csv', 'w')
+    csv_writer = csv.writer(data_file)
 
+    count = 0
+    for d in temp_data:
+        if count == 0:
+            header = d.keys()
+            csv_writer.writerow(header)
+            count += 1
+        
+        csv_writer.writerow(d.values())
+    
+    data_file.close()
+
+    return jsonify(), 200
 
 @app.route('/model_results')
 def model():
+    # We take in the User's Data Station, future date 
+    # Send that data station to this endpoint to get the mindate, and maxdate of the station
+    # www.ncdc.noaa.gov/cdo-web/api/v2/stations/GHCND:USC00203947
+    # Make requests to this endpoint to get back data in a decade for TMAX, TMIN, and TAVG
+    # www.ncdc.noaa.gov/cdo-web/api/v2/data?datatypeid=TAVG&datatypeid=TMIN&datatypeid=TMAX&datasetid=GSOM&startdate=1891-09-01&enddate=1901-09-01&locationid=FIPS:26093&limit=1000&stationid=GHCND:USC00203947"
+    # We want to keep that data type (TMIN,TMAX,TAVG), Date, and Value
+    # 
+    #
     lr = LinearReg('MAR', 2021, 'testing/datastation.csv')
     results = lr.predict()
     print('[INFO] MODEL RESULTS:', results)
