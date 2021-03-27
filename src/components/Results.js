69a93e3cd6e5990a3108c57d7627666091396c12
Killaskt <thegimreper@gmail.com>
2021-03-27 19:20:55 -0400

    nic 3rd prototype changes

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,15 +1,15 @@
-import { Suspense, useCallback, useContext, useEffect, useState } from 'react';
-import './../App.css';
-import { InputsContext, InputsProvider } from '../context/InputsContext';
-import { Table, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { Data } from '@react-google-maps/api';
-import React from 'react';
-import Plot from 'react-plotly.js';
-import Skeleton from '@material-ui/lab/Skeleton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { Suspense, useCallback, useContext, useEffect, useState } from "react";
+import "./../App.css";
+import { InputsContext, InputsProvider } from "../context/InputsContext";
+import { Card, Table, OverlayTrigger, Tooltip, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Data } from "@react-google-maps/api";
+import React from "react";
+import Plot from "react-plotly.js";
+import Skeleton from "@material-ui/lab/Skeleton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
 
-import DataPlot from './Plot';
+import DataPlot from "./Plot";
 
 const Results = ({ getData }) => {
   const { inputs, getModelData, getClusters, getTrainingData } = useContext(
@@ -21,11 +21,11 @@ const Results = ({ getData }) => {
   const [trainingPlotData, setTrainingPlotData] = useState([]);
   const [clusterPlotData, setClusterPlotData] = useState([]);
 
-  const graphLabelStyle = {
-    textAlign: 'Left',
+  const graphLabelStyle = { 
+    textAlign: "Left", 
     fontWeight: 675,
     minWidth: '100px',
-    maxWidth: '200px',
+    maxWidth: '200px' 
   };
 
   useEffect(() => {
@@ -42,46 +42,66 @@ const Results = ({ getData }) => {
   }, [inputs.model]);
 
   useEffect(() => {
+    const generateXvalues = (se, data, key='year') => {
+      var arr = []
+    
+      for(let i = 0; i < data['station'].length; i++) {
+        let d = data[key][i]
+        let idx = se.indexOf(data['station'][i])
+        try {
+          arr[idx].push(d)
+        }
+        catch (e) {
+          arr.push([d])
+        }
+      }
+    
+      return arr // [[x values per station], [] , []...]
+    }
+
     if (
       inputs.cluster.length > 0 &&
       Object.keys(inputs.training_data).length > 0
     ) {
-      setTrainingPlotData([
-        {
-          x: inputs.training_data['year'],
-          y: inputs.training_data[Object.keys(inputs.training_data)[0]],
-          type: 'scatter',
-          text: ['1980'],
-          mode: 'markers',
-          name: 'historical data',
-          marker: { color: 'black' },
-        },
-      ]);
+      const stations = Array.from(new Set(inputs.training_data['training_data']['station']));
+      const xvals = generateXvalues(stations, inputs.training_data['training_data'], inputs.month)
+      const yearvals = generateXvalues(stations, inputs.training_data['training_data'], 'year')
+      const data = xvals.map((data, i) => 
+        ({
+          x: yearvals[i],
+          y: data,
+          type: "scatter",
+          mode: "markers",
+          name: stations[i],
+        })
+      )
+      console.log(new Set(inputs.training_data['training_data']['station']), stations, xvals, data)
+      setTrainingPlotData(data);
+      let cls = inputs.cluster[0]
       setClusterPlotData([
         {
-          x: inputs.cluster[1]['High Temp Plot Cluster'][0],
-          y: inputs.cluster[1]['High Temp Plot Cluster'][1],
-          type: 'scatter',
-          mode: 'markers',
-          text: ['1980'],
-          name: 'High Temps',
-          marker: { color: 'red' },
+          x: cls["High Temp Cluster"][0],
+          y: cls["High Temp Cluster"][1],
+          type: "scatter",
+          mode: "markers",
+          name: "High Temps",
+          marker: { color: "red" },
         },
         {
-          x: inputs.cluster[1]['Mid Temp Plot Cluster'][0],
-          y: inputs.cluster[1]['Mid Temp Plot Cluster'][1],
-          type: 'scatter',
-          mode: 'markers',
-          name: 'Med Temps',
-          marker: { color: 'green' },
+          x: cls["Mid Temp Cluster"][0],
+          y: cls["Mid Temp Cluster"][1],
+          type: "scatter",
+          mode: "markers",
+          name: "Med Temps",
+          marker: { color: "green" },
         },
         {
-          x: inputs.cluster[1]['Low Temp Plot Cluster'][0],
-          y: inputs.cluster[1]['Low Temp Plot Cluster'][1],
-          type: 'scatter',
-          mode: 'markers',
-          name: 'Low Temps',
-          marker: { color: 'blue' },
+          x: cls["Low Temp Cluster"][0],
+          y: cls["Low Temp Cluster"][1],
+          type: "scatter",
+          mode: "markers",
+          name: "Low Temps",
+          marker: { color: "blue" },
         },
       ]);
       setDataRetrieved(true);
@@ -100,50 +120,97 @@ const Results = ({ getData }) => {
     ));
 
   const generateModelDataTable = (temps) => {
+    let labels = ['Low', 'Middle', 'High', 'All Data'];
     return temps.map((data, i) => (
+      data ? 
       <tr key={i}>
-        <td style={graphLabelStyle}>{data?.name}</td>
+        <td style={graphLabelStyle}>{data?.name.split(' ')[0] === 'All' ? 'All Data' : data?.name.split(' ')[0]}</td>
         <td>{data?.prediction[0].toFixed(2)}&#176;C</td>
         <td>{data?.metrics.mse.toFixed(2)}</td>
         <td>{data?.metrics.r2.toFixed(2)}</td>
+      </tr>
+      :
+      <tr key={i}>
+        <td>{labels[i]}</td>
+        <td colSpan="3">Not Enough Data</td>
       </tr>
     ));
   };
 
   const generateClusterTable = (temps, pre, post) => {
-    let tempTitle = ['Low', 'Middle', 'High'];
+    let tempTitle = ["Low", "Middle", "High"];
     let tempCluster = [
-      'Low Temp Cluster',
-      'Mid Temp Cluster',
-      'High Temp Cluster',
+      "Low Temp Cluster",
+      "Mid Temp Cluster",
+      "High Temp Cluster",
     ];
 
+    // console.log(inputs.cluster[1][tempCluster2['Low']][0][inputs.cluster[1][tempCluster2['Low']][1].indexOf(Math.max(...temps['High Temp Cluster']))])  
+    console.log(pre['Pre Low Temp Cluster'])  
     return tempCluster.map((t, i) => (
       <tr key={i}>
         <td style={graphLabelStyle}>{tempTitle[i]}</td>
         <td>
-          {average(temps[t]).toFixed(2)}
+          {average(temps[t][1]).toFixed(2)}
           &#176;C
+          ({temps[t][0][0]} - {temps[t][0][temps[t][0].length - 1]})
         </td>
         <td>
-          {Math.max(...temps[t]).toFixed(2)}
+          {Math.max(...temps[t][1]).toFixed(2)}
           &#176;C
+          ({temps[t][0][temps[t][1].indexOf(Math.max(...temps[t][1]))]})
         </td>
         <td>
-          {Math.min(...temps[t]).toFixed(2)}
+          {Math.min(...temps[t][1]).toFixed(2)}
           &#176;C
+          ({temps[t][0][temps[t][1].indexOf(Math.min(...temps[t][1]))]})
         </td>
         <td>
-          {average(pre['Pre ' + t]).toFixed(2)}
+          {average(pre["Pre " + t][1]).toFixed(2)}
           &#176;C
+          ({pre["Pre " + t][0][0]} - {pre["Pre " + t][0][pre["Pre " + t][0].length - 1]})
         </td>
         <td>
-          {average(post['Post ' + t]).toFixed(2)}
+          {average(post["Post " + t][1]).toFixed(2)}
           &#176;C
+          ({post["Post " + t][0][0]} - {post["Post " + t][0][post["Post " + t][0].length - 1]})
         </td>
       </tr>
     ));
   };
+
+  const generatePrePostTable = (temps) => {
+    const labels = [
+      'Post-1980',
+      'Pre-1980',
+      'All Data'
+    ];
+    console.log(temps['post'][inputs.month])
+    return Object.keys(temps).map((data, i) => 
+      <tr>
+        {console.log(data)}
+        <td>{labels[i]}</td>
+        {/* average */}
+        <td>
+          {average(temps[data][inputs.month]).toFixed(2)}
+          &#176;C
+          ({temps[data]['year'][0]} - {temps[data]['year'][temps[data][inputs.month].length - 1]})
+        </td>
+        {/* highest */}
+        <td>
+          {Math.max(...temps[data][inputs.month]).toFixed(2)}
+          &#176;C
+          ({temps[data]['year'][temps[data][inputs.month].indexOf(Math.max(...temps[data][inputs.month]))]})
+        </td>
+        {/* lowest */}
+        <td>
+          {Math.min(...temps[data][inputs.month]).toFixed(2)}
+          &#176;C
+          ({temps[data]['year'][temps[data][inputs.month].indexOf(Math.min(...temps[data][inputs.month]))]})
+        </td>
+      </tr>
+    )
+  }
 
   const renderR2Tooltip = (props) => (
     <Tooltip {...props}>
@@ -160,11 +227,11 @@ const Results = ({ getData }) => {
   const ModelDataTable = () => {
     return (
       <>
-        <Table responsive="sm">
+        <Table responsive hover>
           <thead>
             <tr>
-              <th> </th>
-              <th>Average Temperature</th>
+              <th>Cluster</th>
+              <th>Average Temperature ({inputs.year})</th>
               <th>
                 MSE
                 <OverlayTrigger
@@ -175,9 +242,9 @@ const Results = ({ getData }) => {
                   <FontAwesomeIcon
                     icon={faQuestionCircle}
                     style={{
-                      marginLeft: '2px',
-                      fontSize: '0.7rem',
-                      verticalAlign: '5px',
+                      marginLeft: "2px",
+                      fontSize: "0.7rem",
+                      verticalAlign: "5px",
                     }}
                   />
                 </OverlayTrigger>
@@ -192,9 +259,9 @@ const Results = ({ getData }) => {
                   <FontAwesomeIcon
                     icon={faQuestionCircle}
                     style={{
-                      marginLeft: '2px',
-                      fontSize: '0.7rem',
-                      verticalAlign: '5px',
+                      marginLeft: "2px",
+                      fontSize: "0.7rem",
+                      verticalAlign: "5px",
                     }}
                   />
                 </OverlayTrigger>
@@ -211,12 +278,12 @@ const Results = ({ getData }) => {
     );
   };
 
-  const ClusterDataTable = useCallback(() => {
-    console.log('ran2');
+  const ClusterDataTable = () => {
+    console.log("ran2");
 
     return (
       <>
-        <Table responsive="sm">
+        <Table responsive hover>
           <thead>
             <tr>
               <th>Clusters</th>
@@ -231,47 +298,92 @@ const Results = ({ getData }) => {
             {inputs.cluster.length > 0
               ? generateClusterTable(
                   inputs.cluster[0],
-                  inputs.cluster[2],
-                  inputs.cluster[3]
+                  inputs.cluster[1],
+                  inputs.cluster[2]
                 )
               : generateSkeleton(3, 6)}
           </tbody>
         </Table>
       </>
     );
-  }, [inputs.cluster, getClusters]);
+  };
+
+  const PrePostTable = () => {
+    console.log("ran3");
+
+    return (
+      <>
+        <Table responsive hover>
+          <thead>
+            <tr>
+              <th> </th>
+              <th>Average temperature</th>
+              <th>Highest Temperature</th>
+              <th>Lowest Temperature</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(inputs.training_data).length > 0
+              ? generatePrePostTable(inputs.training_data)
+              : generateSkeleton(3, 3)}
+          </tbody>
+        </Table>
+      </>
+    );
+  };
 
   const getPlot = useCallback(
     (data, layout) => {
-      return dataRetrieved ? <DataPlot data={data} layout={layout} /> : '';
+      return dataRetrieved ? <DataPlot data={data} layout={layout} /> : "";
     },
     [inputs.training_data, inputs.cluster, dataRetrieved]
   );
 
   return (
     <div className="results">
-      <h2> {`${inputs.county}, ${inputs.state} Results`} </h2>
-      <div className="table-plots">
-        <div className="tables">
-          <div className="linearTable">
-            <h4>Linear Model Predictions</h4>
-            <ModelDataTable />
-          </div>
-
-          <div className="clusterModelTable">
-            <h4>Cluster Model Calculations</h4>
-            <p>
-              This chart shows the median, highest, and lowest temperatures of
-              the year
-            </p>
-            <ClusterDataTable />
-          </div>
+      <h2> {`${inputs.county}, ${inputs.state} Profile`} </h2>
+      <div className="tables">
+        <div className="linearTable">
+          <Card>
+            <Card.Header as='h6'>Linear Model Predictions</Card.Header>
+            <Card.Body style={{padding: '.3em 1.25em 0px'}}>
+              <ModelDataTable />
+            </Card.Body>
+          </Card>
         </div>
+        <div className="clusterModelTable">
+          <Card>
+            <Card.Header>Cluster Model Calculations</Card.Header>
+            <Card.Body style={{padding: '.75em 1.25rem 0'}}>
+              <Card.Subtitle>
+              <ListGroup>
+                <ListGroupItem>
+                  This chart shows the median, highest, and lowest temperatures of the year.
+                </ListGroupItem>
+              </ListGroup>
+              </Card.Subtitle>
+              <Card.Text>
+                <ClusterDataTable />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="PrePostTable">
+          <Card>
+            <Card.Header>Pre-Post-All Data Statistics</Card.Header>
+            <Card.Body style={{padding: '.75em 1.25rem 0'}}>
+              <Card.Text>
+                <PrePostTable />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+
         <div className="plots">
           <div className="histTempDataPlot">
             {trainingPlotData.length > 0 ? (
               getPlot(trainingPlotData, {
-                title: 'Historical Temperature Data',
+                title: "Historical Temperature Data",
               })
             ) : (
               <Skeleton variant="rect" width={500} height={500} />
@@ -280,7 +392,7 @@ const Results = ({ getData }) => {
           <div className="histRangeTempsPlot">
             {clusterPlotData.length > 0 ? (
               getPlot(clusterPlotData, {
-                title: 'Clustered Historical Temperatures',
+                title: "Clustered Historical Temperatures",
               })
             ) : (
               <Skeleton variant="rect" width={500} height={500} />
