529c1dfdb96cab013d91d2ef8fcdb461e6345bf2
yanassss <s.sunayanasharma@gmail.com>
2021-03-29 19:20:07 -0400

    Created About page and added app info

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useContext, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+import About from './components/About';
 import Form from './components/Form';
 import Map from './components/Map';
 import ErrorMessage from './components/ErrorMessage';
@@ -39,82 +41,91 @@ function App() {
   const onSubmitPressed = () => setShowResults(true);
 
   return (
-    <div className="App">
-      <div className="default-view-container">
-        <div className="default-view">
-          <div className="title-logo">
-            <div className="title">
-              <FontAwesomeIcon
-                className={spin ? 'fa-spin' : ''}
-                onMouseLeave={toggleHover}
-                onMouseEnter={toggleHover}
-                icon={faGlobeAmericas}
-                style={{ fontSize: '2.7rem', verticalAlign: '0rem' }}
-              />
-              <h1 style={{ paddingLeft: '10px' }}>RAFT</h1>
-            </div>
-            {/* <div className="logo">
+    <Router>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <div className="App">
+            <div className="default-view-container">
+              <div className="default-view">
+                <div className="title-logo">
+                  <div className="title">
+                    <FontAwesomeIcon
+                      className={spin ? 'fa-spin' : ''}
+                      onMouseLeave={toggleHover}
+                      onMouseEnter={toggleHover}
+                      icon={faGlobeAmericas}
+                      style={{ fontSize: '2.7rem', verticalAlign: '0rem' }}
+                    />
+                    <h1 style={{ paddingLeft: '10px' }}>RAFT</h1>
+                  </div>
+                  {/* <div className="logo">
             </div> */}
-          </div>
-          <div className="hero">
-            <div className="hero-container">
-              <div className="information">
-                <h3 style={{ paddingBottom: '.5rem' }}>
-                  Regional Temperature Profiler
-                </h3>
-                <p>
-                  RTP is an application that uses a linear regression model that{' '}
-                  <br />
-                  will help you find a predicted temperature in a chosen county
-                  and year.
-                </p>
-                <p>Choose a state to get started!</p>
-              </div>
-              <div className="inner-hero-cont">
-                <Form
-                  getData={getData}
-                  setGetData={setGetData}
-                  onSubmitPressed={onSubmitPressed}
-                  setOpen={setOpen}
-                  open={open}
-                />
-                <div className="map">
-                  <Map />
+                </div>
+                <div className="hero">
+                  <div className="hero-container">
+                    <div className="information">
+                      <h3 style={{ paddingBottom: '.5rem' }}>
+                        Regional Temperature Profiler
+                      </h3>
+                      <p>
+                        RTP is an application that uses a linear regression
+                        model that <br />
+                        will help you find a predicted temperature in a chosen
+                        county and year.
+                      </p>
+                      <p>Choose a state to get started!</p>
+                    </div>
+                    <div className="inner-hero-cont">
+                      <Form
+                        getData={getData}
+                        setGetData={setGetData}
+                        onSubmitPressed={onSubmitPressed}
+                        setOpen={setOpen}
+                        open={open}
+                      />
+                      <div className="map">
+                        <Map />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <div id="results">
+                {showResults ? <Results getData={getData} /> : ''}
+              </div>
             </div>
-          </div>
-        </div>
-        {/* <div id="results"> */}
-          {showResults ? <Results getData={getData} /> : ''}
-        {/* </div> */}
-      </div>
-      <footer
-        style={{
-          backgroundColor: '#f8f8ff',
-          height: '50px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <p style={{ margin: '0', paddingRight: '10px' }}>&#169; 2021</p>
-        <a href="https://www.lhei.org">lhei.org</a>
-        <a style={{ paddingLeft: '10px' }} href="#">
-          About Link
-        </a>
-      </footer>
+            <footer
+              style={{
+                backgroundColor: '#f8f8ff',
+                height: '50px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <p style={{ margin: '0', paddingRight: '10px' }}>&#169; 2021</p>
+              <a href="https://www.lhei.org">lhei.org</a>
+              <a style={{ paddingLeft: '10px' }} href="/about">
+                About Link
+              </a>
+            </footer>
 
-      {open ? (
-        <ErrorMessage
-          setOpen={setOpen}
-          open={open}
-          message={inputs.errorMessage}
-        />
-      ) : (
-        ''
-      )}
-    </div>
+            {open ? (
+              <ErrorMessage
+                setOpen={setOpen}
+                open={open}
+                message={inputs.errorMessage}
+              />
+            ) : (
+              ''
+            )}
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,60 @@
+import { useContext, useEffect, useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import './About.css';
+
+function About() {
+  const [open, setOpen] = useState(false);
+  const [spin, setSpin] = useState(false);
+
+  const toggleHover = () => setSpin(!spin);
+
+  return (
+    <div className="App">
+      <div className="default-view-container">
+        <div className="default-view">
+          <div className="title-logo">
+            <div className="title">
+              <FontAwesomeIcon
+                className={spin ? 'fa-spin' : ''}
+                onMouseLeave={toggleHover}
+                onMouseEnter={toggleHover}
+                icon={faGlobeAmericas}
+                style={{ fontSize: '2.7rem', verticalAlign: '0rem' }}
+              />
+              <h1 style={{ paddingLeft: '10px' }}>RAFT</h1>
+            </div>
+          </div>
+
+          <div className="description">
+            <h3 style={{ paddingBottom: '.5rem' }}>
+              Regional Temperature Profiler
+            </h3>
+            <p>
+              RTP is a web application that will accurately predict future
+              temperature values of regions in the United States of America
+              based on user input. The user will input the state, county, future
+              year, and month for which he would like to see the predicted
+              temperature. <br />
+              <p> </p>
+              The data will be input into the data model which will produce the
+              machine learning model predictions, cluster model calculations,
+              and average and record temperatures of the chosen county to the
+              user. <br /> <p> </p>Graphs showing historical temperature data
+              and historical high, middle, and low range temperatures will also
+              be displayed. The data being aggregated comes from NOAA (National
+              Oceanic and Atmospheric Administration). <br /> <p> </p>
+              Data will be collected from data stations in the county that the
+              user selects. The most important data being collected are
+              location, station name, region, and temperatures.
+            </p>
+          </div>
+          <div>{/* <img src=“rtpLogo.jpg" alt="RTP"> */}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
